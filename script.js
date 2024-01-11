cc07f520246d67f4b8622edc359644f35339e12a
URMIK J PATEL <114993425+PRABHU-ujp6@users.noreply.github.com>
2024-01-11 11:18:01 +0530

    updated

--- script.js
+++ script.js
@@ -28,17 +28,17 @@ async function checkWeather(city){
         //changing of icons with respect to city's weather 
 
         if(data.weather[0].main == "Clouds"){
-            weatherIcon.src = "/images/clouds.png";
+            weatherIcon.src = "images/clouds.png";
         }else if(data.weather[0].main == "Clear") {
-            weatherIcon.src = "/images/clear.png";
+            weatherIcon.src = "images/clear.png";
         }else if(data.weather[0].main == "Rain") {
-            weatherIcon.src = "/images/rain.png";
+            weatherIcon.src = "images/rain.png";
         }else if(data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "/images/drizzle.png";
+            weatherIcon.src = "images/drizzle.png";
         }else if(data.weather[0].main == "Mist") {
-            weatherIcon.src = "/images/mist.png";
+            weatherIcon.src = "images/mist.png";
         }else if(data.weather[0].main == "Snow") {
-            weatherIcon.src = "/images/snow.png";
+            weatherIcon.src = "images/snow.png";
         }
     
         //if valid input is entered...
